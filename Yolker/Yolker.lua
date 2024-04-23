6169c79ea08204809807cd908332381a6f29971a
UncleEnk <81113282+UncleEnk@users.noreply.github.com>
2024-04-22 20:19:17 -0700

    Added Yolker (low effort)

---
+++ Yolker/Yolker.lua
@@ -0,0 +1,15 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Yolker
+--- MOD_ID: Yolker
+--- MOD_AUTHOR: [uncleenk]
+--- MOD_DESCRIPTION: Changes localization to turn "egg" into "Yolker"
+
+----------------------------------------------
+------------MOD CODE -------------------------
+
+function SMODS.INIT.Yolker()
+    G.localization.descriptions.Joker.j_egg.name = "Yolker"
+end
+
+------------MOD CODE END----------------------
+----------------------------------------------
